bb2e953e813a754a6768590c926606c2b0ee9437
LordSk <lordskelethom@gmail.com>
2018-09-19 17:02:01 +0200

    dota picker player position heroes page

--- dota_picker/dota_picker.js
+++ dota_picker/dota_picker.js
@@ -1,5 +1,77 @@
 //
 
+let blockTeam = $('.head_team');
+let blockPlayer = $('.head_player');
+
+$('#tab_team').click(function() {
+    setupTabTeam();
+});
+
+$('#tab_player').click(function() {
+    setupTabPlayer();
+});
+
+let hero_list = {};
+let selected_list = [];
+let div_selectedList = $('.head_team .selected_hero_list');
+
+function setupTabTeam()
+{
+    //console.log("Setting team page");
+    blockTeam.show();
+    blockPlayer.hide();
+
+    $('div.hero_list .hero_button').unbind();
+    $('div.hero_list .hero_button').click(function() {
+        if(!$(this).hasClass('grayed') && selected_list.length < 5) {
+            selected_list.push(($(this).attr('name')));
+            updateSelectedList();
+        }
+    });
+
+    saveUrlTeam();
+}
+
+let playerPosSelectedId = 0;
+let posSelectedHeroList = [];
+posSelectedHeroList[0] = [];
+posSelectedHeroList[1] = [];
+posSelectedHeroList[2] = [];
+posSelectedHeroList[3] = [];
+posSelectedHeroList[4] = [];
+
+let div_posSelectedList = [];
+div_posSelectedList[0] = $('.head_player #pos1 .selected_hero_list');
+div_posSelectedList[1] = $('.head_player #pos2 .selected_hero_list');
+div_posSelectedList[2] = $('.head_player #pos3 .selected_hero_list');
+div_posSelectedList[3] = $('.head_player #pos4 .selected_hero_list');
+div_posSelectedList[4] = $('.head_player #pos5 .selected_hero_list');
+
+function setupTabPlayer()
+{
+    //console.log("Setting player page");
+    blockTeam.hide();
+    blockPlayer.show();
+
+    $('.pos_column').first().addClass('selected');
+    $('.pos_column').click(function() {
+        playerPosSelectedId = $(this).index();
+
+        $('.pos_column').removeClass('selected');
+        $('.pos_column').eq(playerPosSelectedId).addClass('selected');
+    });
+
+    $('div.hero_list .hero_button').unbind();
+    $('div.hero_list .hero_button').click(function() {
+        if(!$(this).hasClass('grayed') && posSelectedHeroList[playerPosSelectedId].length < 5) {
+            posSelectedHeroList[playerPosSelectedId].push(($(this).attr('name')));
+            updatePositionSelectedList(playerPosSelectedId);
+        }
+    });
+
+    saveUrlPlayerPosition();
+}
+
 // make progress bars
 $('.progress_bar').each(function() {
     $(this).append('<div class="bar"></div>');
@@ -16,7 +88,7 @@ const roles = {
     Durable: 0,
 }
 
-var filter = {
+let filter = {
     search: '',
     roles: {
         Carry: 0,
@@ -30,19 +102,15 @@ var filter = {
     }
 };
 
-var hero_list = {};
-var selected_list = [];
-var div_selectedList = $('div.selected_hero_list');
-
 // load hero data from page html
 $('div.hero_list .hero_button').each(function() {
-    var thisRolesArray = $(this).attr('roles').split(' ');
-    var thisRoles = {};
+    let thisRolesArray = $(this).attr('roles').split(' ');
+    let thisRoles = {};
 
-    for(var r in roles) {
+    for(let r in roles) {
         thisRoles[r] = 0;
 
-        for(var i = 0; i < thisRolesArray.length; i +=1) {
+        for(let i = 0; i < thisRolesArray.length; i +=1) {
             if(thisRolesArray[i] == r) {
                 thisRoles[r] = 1;
                 break;
@@ -57,26 +125,42 @@ $('div.hero_list .hero_button').each(function() {
     }
 });
 
-// load heroes from url
-var a = window.location.search.split('=');
-if(a[0] == '?heroes' && a[1].length > 0) {
-    var urlHeroList = a[1].split(',');
+// load from url
+let a = window.location.search.split('=');
+if(a[0] == '?heroes') {
+    setupTabTeam();
 
-    selected_list = selected_list.concat(urlHeroList);
-    updateSelectedList();
-}
+    if(a[1].length > 0) {
+        let urlHeroList = a[1].split(',');
 
-$('div.hero_list .hero_button').click(function() {
-    if(!$(this).hasClass('grayed') && selected_list.length < 5) {
-        selected_list.push(($(this).attr('name')));
+        selected_list = selected_list.concat(urlHeroList);
         updateSelectedList();
     }
-});
+}
+// load position heroes
+else if(a[0] == '?p1') {
+    setupTabPlayer();
+
+    a = window.location.search.split('&');
+    a[0] = a[0].substring(1);
+
+    for(let i = 0; i < a.length; i++) {
+        let b = a[i].split('=');
+        if(b[0] == 'p' + (i+1) && b[1].length > 0) {
+            let urlHeroList = b[1].split(',');
+            posSelectedHeroList[i] = posSelectedHeroList[i].concat(urlHeroList);
+            updatePositionSelectedList(i);
+        }
+    }
+}
+else {
+    setupTabTeam();
+}
 
 function updateSelectedList()
 {
-    var html = '';
-    var roleCount = {
+    let html = '';
+    let roleCount = {
         Carry: 0,
         Support: 0,
         Nuker: 0,
@@ -87,35 +171,77 @@ function updateSelectedList()
         Durable: 0,
     };
 
-    for(var i = 0; i < selected_list.length; i += 1) {
+    for(let i = 0; i < selected_list.length; i += 1) {
         if(!hero_list.hasOwnProperty(selected_list[i])) continue;
-        var hero = hero_list[selected_list[i]];
+        let hero = hero_list[selected_list[i]];
         html += '<div class="hero_button"><img src="' + hero.src + '"></div>';
 
-        for(var r in hero.roles) {
+        for(let r in hero.roles) {
             roleCount[r] += hero.roles[r];
         }
     }
     div_selectedList.html(html);
 
-    $('div.selected_hero_list .hero_button').click(function() {
+    div_selectedList.children('.hero_button').click(function() {
         if(selected_list.length > 0) {
-            var index = $(this).index();
+            let index = $(this).index();
             selected_list.splice(index, 1);
             updateSelectedList();
         }
     });
 
-    // save state in url
-    history.pushState(null, '', '?heroes=' + selected_list.join(','));
-
-    for(var r in roleCount) {
+    
+    for(let r in roleCount) {
         $('.progress_bar#pb_' + r + ' .bar').each(function() {
-            var val = roleCount[r] / 5.0 * 100.0;
+            let val = roleCount[r] / 5.0 * 100.0;
             $(this).attr('val', val);
             $(this).css('width', val + '%');
         });
     }
+
+    saveUrlTeam();
+}
+
+function saveUrlTeam()
+{
+    // save state in url
+    history.pushState(null, '', '?heroes=' + selected_list.join(','));
+}
+
+function updatePositionSelectedList(posId)
+{
+    let sellist = posSelectedHeroList[posId];
+    let html = '';
+    
+    for(let i = 0; i < sellist.length; i += 1) {
+        if(!hero_list.hasOwnProperty(sellist[i])) continue;
+        let hero = hero_list[sellist[i]];
+        html += '<div class="hero_button"><img src="' + hero.src + '"></div>';
+    }
+    div_posSelectedList[posId].html(html);
+
+    div_posSelectedList[posId].children('.hero_button').click(function() {
+        if(sellist.length > 0) {
+            let index = $(this).index();
+            sellist.splice(index, 1);
+            updatePositionSelectedList(posId);
+        }
+    });
+
+    saveUrlPlayerPosition();
+}
+
+function saveUrlPlayerPosition()
+{
+    // save state in url
+    let url = '?'
+    for(let i = 0; i < 5; i += 1) {
+        url += 'p' + (i+1);
+        url += '=' + posSelectedHeroList[i].join(',');
+        url += '&';
+    }
+
+    history.pushState(null, '', url);
 }
 
 
@@ -133,8 +259,8 @@ $('.tag_select li').click(function() {
 function applyFilter()
 {
     if(filter.search == '') {
-        var empty = true;
-        for(var r in filter.roles) {
+        let empty = true;
+        for(let r in filter.roles) {
             if(filter.roles[r] != 0) {
                 empty = false;
                 break;
@@ -149,14 +275,14 @@ function applyFilter()
         }
     }
 
-    var searchList = filter.search.length > 0 ? filter.search.toLowerCase().split(' '): [];
-    var heroFiltered = {};
+    let searchList = filter.search.length > 0 ? filter.search.toLowerCase().split(' '): [];
+    let heroFiltered = {};
 
-    for(var h in hero_list) {
+    for(let h in hero_list) {
         heroFiltered[h] = 0;
 
         if(searchList.length > 0) {
-            for(var i = 0; i < searchList.length; i += 1) {
+            for(let i = 0; i < searchList.length; i += 1) {
                 if(hero_list[h].search.search(searchList[i]) != -1) {
                     heroFiltered[h] = 1;
                     break;
@@ -168,7 +294,7 @@ function applyFilter()
         }
 
         if(heroFiltered[h] == 1) {
-            for(var r in filter.roles) {
+            for(let r in filter.roles) {
                 if(filter.roles[r] == 1 && hero_list[h].roles[r] != filter.roles[r]) {
                     heroFiltered[h] = 0;
                     break;
@@ -182,7 +308,7 @@ function applyFilter()
         $(this).addClass('grayed');
     });
 
-    for(var h in heroFiltered) {
+    for(let h in heroFiltered) {
         if(heroFiltered[h] == 1) {
             $('.hero_list .hero_button[name="' + h + '"]').each(function() {
                 $(this).removeClass('grayed');
